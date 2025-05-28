f62c7b6a964e9b908d6e72bb6cf1e2ca28895bb5
HilmaZahra <hzqorina28@gmail.com>
2025-05-29 01:01:30 +0700

    pe

---
+++ resources/views/livewire/blog.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <div class="h-screen bg-cover bg-center relative" style="background-image: url('{{ asset('images/blog-banner.jpg') }}')">
+        <div class="absolute bottom-0 left-0 mx-20 mb-20 text-white space-y-5">
+            <h4 class="text-2xl font-semibold">Business Strategy</h4>
+            <h2 class="text-5xl font-bold">How District Plans Its Next Big Moves</h2>
+            <p class="text-lg text-justify">Streetwear may look effortless—but behind every drop, there’s a team analyzing charts, planning campaigns, and breaking down market trends. At District, we take strategy seriously. Our core team regularly meets to review everything—from customer feedback to seasonal buying behavior—so every collection we release isn’t just stylish, but timed and targeted.</p>
+        </div>
+    </div>
+
+    <div class="bottom-0 left-0 mx-20 my-20">
+        <h2 class="text-3xl font-bold" style="color: #16302B">Blog Post</h2>
+        <div class="my-10 grid grid-cols-3 gap-6">
+        <x-post-card></x-post-card>
+        <x-post-card></x-post-card>
+        <x-post-card></x-post-card>
+        <x-post-card></x-post-card>
+        <x-post-card></x-post-card>
+        <x-post-card></x-post-card>
+        </div>
+    </div>
+</div>
+
