9931c977ff3d736acf73e96e364a2add07b92ee9
Akhdilputra_ <49122160+RaffiAkhdilputra@users.noreply.github.com>
2025-05-31 00:17:37 +0700

    shop + pagination (blm bener kalo gaada index item yang dijual)

--- resources/views/livewire/shop.blade.php
+++ resources/views/livewire/shop.blade.php
@@ -1,3 +1,32 @@
-<div>
-    {{-- Stop trying to control. --}}
+@php
+    $currentPage = 1;
+    $totalPages = 10;
+@endphp
+
+<div class="flex flex-col items-center justify-center">
+    <div class="flex flex-row w-[85%] py-10">
+        <div class="w-1/4">
+            <div class="flex flex-col space-y-5 pr-20">
+                <h3 class="text-3xl text-[#16302B] font-bold">Filter Options</h3>
+                <form wire:submit.prevent="filter">
+                    <div class="flex flex-col space-y-2">
+                        <x-form-input type="text" id="search" name="search" placeholder="Search" />
+                        <x-form-input type="text" id="category" name="category" placeholder="Category" />
+                        <x-form-input type="text" id="color" name="color" placeholder="Color" />
+                        <x-form-input type="text" id="size" name="size" placeholder="Size" />
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="flex flex-col items-center justify-center w-3/4 h-[full + 100px] ">
+            <div class="grid grid-cols-3 gap-5">
+                @foreach (range(1, 12) as $i)
+                    <div class="my-3">
+                        <a href=""><x-card/></a>
+                    </div>
+                @endforeach
+            </div>
+            <x-pagination :currentPage="$currentPage" :totalPages="$totalPages" />
+        </div>
+    </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function () {
     Route::get('/', App\Livewire\Index::class)->name('index');
     Route::get('/blog', App\Livewire\Blog::class)->name('blog');
-    Route::get('/shop', App\Livewire\Shop::class)->name('shop');
+    Route::get('/shop/page-{currentPage}', App\Livewire\Shop::class)->name('shop');
     Route::get('/profile/{user}', App\Livewire\Profile::class)->name('profile.{user}');
 });
 
@@ -18,7 +18,7 @@ Route::middleware(['guest'])->group(function () {
 Route::get('/', App\Livewire\Index::class)->name('index');
 Route::get('/profile/user/{name}', App\Livewire\Profile::class)->name('profile.{user}');
 Route::get('/blog', App\Livewire\Blog::class)->name('blog');
-Route::get('/shop', App\Livewire\Shop::class)->name('shop');
+Route::get('/shop/page-{currentPage}', App\Livewire\Shop::class)->name('shop');
 
 
 // Route hanya untuk user yang sudah login
