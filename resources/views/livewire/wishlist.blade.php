fc73b9fa3063aa8022e17a7cc314cf7bf22d7add
HilmaZahra <hzqorina28@gmail.com>
2025-06-09 06:50:45 +0700

    ok

---
+++ resources/views/livewire/wishlist.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <div class="mx-40 my-10">
+        <div class="flex justify-between">
+            <div class="content-center">
+                <x-button-secondary bgColor="#16302B" width="38">Edit</x-button-secondary>
+            </div>
+            <div class="content-center">
+                <x-search-bar></x-search-bar>
+            </div>
+        </div>
+    </div>
+
+    <div class="mx-40 my-20">
+        <div class="grid grid-cols-3 gap-12 justify-items-center">
+            <x-card></x-card>
+            <x-card></x-card>
+            <x-card></x-card>
+            <x-card></x-card>
+            <x-card></x-card>
+            <x-card></x-card>
+            <x-card></x-card>
+            <x-card></x-card>
+            <x-card></x-card>
+        </div>
+    </div>
+</div>
