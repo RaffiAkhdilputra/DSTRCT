b8b53ecc87d3eeed6b6df54a3e46aabaee660321
HilmaZahra <hzqorina28@gmail.com>
2025-06-12 06:30:09 +0700

    ok

--- resources/views/livewire/payment.blade.php
+++ resources/views/livewire/payment.blade.php
@@ -1,4 +1,4 @@
-<div class="mx-40 my-20">
+<div class="mx-40 my-10">
     <div class="space-y-8">
         <div class="flex rounded-lg bg-[#16302B] text-white p-6 gap-4">
             <div>
@@ -31,24 +31,69 @@
                     </div>
                 </div>
             </div>
-            <div class="space-y-6">
+            <div class="space-y-7">
                 <p class="text-[#A38560]">*This is a limited edition item, so make sure you've selected the right size and style before checking out.</p>
-                <div class="flex items-center gap-4">
-                    <div class="w-10 h-10 bg-[#16302B] rounded-md text-white flex items-center justify-center">
-                        <p class="text-lg font-bold">1</p>
-                    </div>
-                    <div class="">
-                        <p class="font-semibold">Black</p>
-                        <p class="font-semibold">XL</p>
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="flex items-center gap-4">
+                        <div class="w-10 h-10 bg-[#16302B] rounded-md text-white flex items-center justify-center">
+                            <p class="text-lg font-bold">1</p>
+                        </div>
+                        <div>
+                            <p class="font-semibold">Black</p>
+                            <p class="font-semibold">XL</p>
+                        </div>
                     </div>
                     <div class="text-right">
                         <p class="text-3xl font-bold">Rp. 100.000</p>
                     </div>
                 </div>
+                <div class="grid grid-cols-2 gap-4 p-4 bg-[#16302B] text-white">
+                    <div class="flex items-center gap-4">
+                        <p>%</p>
+                        <p>Apply available vouchers</p>
+                    </div>
+                    <div class="justify-self-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="grid grid-cols-2">
+                    <div class="space-y-1 font-semibold">
+                        <p>Subtotal</p>
+                        <div class="flex items-center gap-2">
+                            <p>Order Fees</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                                <path fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-7-4a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM9 9a.75.75 0 0 0 0 1.5h.253a.25.25 0 0 1 .244.304l-.459 2.066A1.75 1.75 0 0 0 10.747 15H11a.75.75 0 0 0 0-1.5h-.253a.25.25 0 0 1-.244-.304l.459-2.066A1.75 1.75 0 0 0 9.253 9H9Z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <p class="font-bold">Total Payment</p>
+                    </div>
+                    <div class="space-y-1 justify-self-end text-right font-semibold">
+                        <p>Rp. 100.000</p>
+                        <p>Rp. 100.000</p>
+                        <p class="font-bold text-lg">Rp. 100.000</p>
+                    </div>
+                </div>
+                <div class="grid grid-cols-2 gap-4 p-4 bg-[#16302B] text-white">
+                    <div class="flex items-center gap-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
+                            <path fill-rule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd" />
+                        </svg>
+                        <p>Select Payment Method</p>
+                    </div>
+                    <div class="justify-self-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </div>
+                </div>
+
             </div>
         </div>
-        <div>
-            <x-button-secondary>Buy</x-button-secondary>
+        <div class="justify-items-center">
+            <x-button-secondary width="52" heigth="16" rounded="3xl">Buy Rp. 100.000</x-button-secondary>
         </div>
     </div>
 </div>
